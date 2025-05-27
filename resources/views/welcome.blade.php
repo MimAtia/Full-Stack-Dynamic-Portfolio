e05324d4cbf27420aaeaf85bcd66d1c7d522b5f8
MimAtia <mim15-5375@diu.edu.bd>
2025-05-27 17:29:31 +0600

    lab3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Atia Sultana Mim</title>
+  <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 </head>
 <body>
-    <h1> 
-        Welcome from laravel
-    </h1>
+  <div class="header">
+  <div class="nav">
+    
+      <a href="index.html">Home</a>
+      <a href="personal.html">Personal</a>
+      <a href="education.html">Education</a>
+      <a href="skills.html">Skills</a>
+      <a href="contact.html">Contact</a>
+    </div>
 
+    <div>
+    <h1>Atia Sultana Mim</h1>
+    <p>Student of Computer Science and Engineering</p>
+
+    </div>
+
+  </div>
+
+  <div class="intro">
+    <div class="image">
+      <img src="{{ asset('assets\images\mim (2).png') }}">
+    </div>
+
+    <div class="test">
+      <h2>Hello there,</h2>
+      <p>I am <b>Atia Sultana Mim</b>.</p>
+      <p>Welcome to my website. Here you can get information about me. And thank you for visiting.</p>
+      <p>I am currently a student at Daffodil International University. I am studying B.Sc. in Computer Science and Engineering.</p>
+      <a href ="#readmore">Readmore</a>
+    </div>
+  </div>
 </body>
 </html>
